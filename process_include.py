e41a8fcde76f5987b6cf482bd6e20f31b03a9a44
David Gessner <davidges@gmail.com>
2015-07-27 13:46:55 +0200

    Rename variable

--- process_include.py
+++ process_include.py
@@ -5,12 +5,12 @@ import os
 
 prism_file_path = sys.argv[1]
 prism_file = open(prism_file_path, "r")
-prism_dir = os.path.dirname(prism_file_path)
+prism_dirname = os.path.dirname(prism_file_path)
 
 for line in prism_file:
     if "INCLUDE " in line:
         included_file_path = line.split(" ")[1].rstrip()
-        included_file = open(prism_dir + "/" + included_file_path, "r")
+        included_file = open(prism_dirname + "/" + included_file_path, "r")
         for included_line in included_file:
             print included_line,
     else:
