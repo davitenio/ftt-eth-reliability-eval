95adda060427420cd31074a086a28b7657925820
David Gessner <davidges@gmail.com>
2015-09-09 12:33:22 +0200

    Add function to draw CTMC digraph

--- graph-based-eval/generatectmc.py
+++ graph-based-eval/generatectmc.py
@@ -2,6 +2,42 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
+def save_ctmc_drawing(ctmc_graph, filename, labels=None, graph_layout='spring',
+               node_size=1600, node_color='gray', node_alpha=0.3,
+               node_text_size=12,
+               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               edge_text_pos=0.3,
+               text_font='sans-serif'):
+
+    figure, axis = plt.subplots()
+
+    if graph_layout == 'shell':
+        graph_pos=nx.shell_layout(ctmc_graph)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(ctmc_graph)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(ctmc_graph)
+    else:
+        graph_pos=nx.spring_layout(ctmc_graph)
+
+    state_labels = {}
+    for state_graph in ctmc_graph.nodes():
+        state_labels[state_graph] = state_graph.nodes()
+
+    nx.draw_networkx_nodes(ctmc_graph, graph_pos, node_size=node_size,
+                           alpha=node_alpha, node_color=node_color, ax=axis)
+    nx.draw_networkx_edges(ctmc_graph, graph_pos, width=edge_tickness,
+                           alpha=edge_alpha, edge_color=edge_color,
+                           arrows=True, ax=axis)
+    nx.draw_networkx_labels(ctmc_graph, graph_pos, font_size=node_text_size,
+                            font_family=text_font, ax=axis)
+    nx.draw_networkx_labels(ctmc_graph, graph_pos, state_labels)
+
+    plt.axis('off')
+
+    plt.savefig(filename)
+
+
 def save_graph_drawing(graph, filename, labels=None, graph_layout='spring',
                node_size=1600, node_color='blue', node_alpha=0.3,
                node_text_size=12,
