dbd6017d6d3bd33512ea5714f2c7ac9c532bae3d
David Gessner <davidges@gmail.com>
2016-01-17 15:59:48 +0100

    Add missing entries to failure_mode_mutation_probabilities

--- graph-based-eval/fttrs.py
+++ graph-based-eval/fttrs.py
@@ -238,6 +238,24 @@ failure_mode_mutation_probabilities = {
                 'byzantine': 1 - 0.333
             }
         },
+        switches: {
+            'crash': {
+                # Probability of a switch crashing when it cannot contain a
+                # crash from an adjacent port.
+                'crash': 1,
+                # Probability of a switch becoming byzantine when it cannot
+                # contain a crash from an adjacent port.
+                'byzantine': 0
+            },
+            'byzantine': {
+                # Probability of a switch crashing when it cannot contain a
+                # byzantine from an adjacent port.
+                'crash': 1,
+                # Probability of a switch becoming byzantine when it cannot
+                # contain a byzantine from an adjacent port.
+                'byzantine': 0
+            }
+        },
         links: {
             'crash': {
                 # Probability of a link crashing when it cannot contain a
@@ -277,7 +295,6 @@ failure_mode_mutation_probabilities = {
                 'byzantine': 1
             }
         },
-
         guardians: {
             'crash': {
                 # Probability of a guardian crashing when it cannot contain a
@@ -337,9 +354,26 @@ failure_mode_mutation_probabilities = {
         }
     },
 
-
     # Guardians have crash failure semantics
     switches: {
+        ports: {
+            'crash': {
+                # Probability of a port crashing when it cannot contain a
+                # crash from an adjacent switch.
+                'crash': 0.111,
+                # Probability of a port becoming byzantine when it cannot
+                # contain a crash from an adjacent switch.
+                'byzantine': 1 - 0.111
+            },
+            'byzantine': {
+                # Probability of a port crashing when it cannot contain a
+                # byzantine from an adjacent switch.
+                'crash': 0,
+                # Probability of a port becoming byzantine when it cannot
+                # contain a byzantine from an adjacent switch.
+                'byzantine': 1
+            }
+        },
         guardians: {
             'crash': {
                 # Probability of a guardian crashing when it cannot contain a
